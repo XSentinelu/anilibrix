e6ae6bfae3245113f376128af42cce5893c6d97a
viiprogrammer <m_telega@mail.ru>
2023-10-15 19:17:55 +0300

    fix: arrow buttons forward / rewind

--- src/renderer/components/video/interface/components/keyboard/PlayerKeyboard.vue
+++ src/renderer/components/video/interface/components/keyboard/PlayerKeyboard.vue
@@ -68,8 +68,10 @@ export default {
       if (e.code === 'KeyF') this.$emit('toggle:fullscreen') // F -> fullscreen
 
       // Seek
-      if (e.which === 39 || e.code === 'MediaTrackNext') toVideo(this.release, this.next, {fromStart: true})
-      if (e.which === 37 || e.code === 'MediaTrackPrevious') toVideo(this.release, this.previous, {fromStart: true})
+      if (e.which === 39) this.forward() // right arrow -> forward
+      if (e.which === 37) this.rewind() // left arrow -> rewind
+      if (e.code === 'MediaTrackNext') toVideo(this.release, this.next, {fromStart: true})
+      if (e.code === 'MediaTrackPrevious') toVideo(this.release, this.previous, {fromStart: true})
 
       // Volume
       if (e.which === 38 || e.code === 'VolumeUp') {
@@ -80,6 +82,7 @@ export default {
           this.$emit('set:volume', newVolume)
         }
       } // up arrow -> inc vol
+
       if (e.which === 40 || e.code === 'VolumeDown') {
         if (this.player.volume <= 0.05) {
           this.$emit('set:volume', 0)
